ad2e04043de5c7a5a1bce28d0878adb7a17379af
MamadHp <muhammadhoseinpour@gmail.com>
2026-09-14 02:21:21 +0330

    Update VERSION to 2.1.2 in install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ NC="\033[0m"
 # ==========================================
 # Variables
 # ==========================================
-VERSION="1.1.6"
+VERSION="2.1.2"
 WORK_DIR="/root/client_bot"
 SERVICE_NAME="client_bot"
 FILE_NAME="client_bot.bin" # نام دقیق فایل باینری آپلود شده در گیت‌هاب
